a32b51194ab168e5d7ba8a8d6a94122bfa1dc0d8
Nikhil Warge <155173949+Nikhilwarge1999@users.noreply.github.com>
2024-12-26 01:26:55 +0530

    add new stopwords

--- NLP/text processing using Lemmitization.py
+++ NLP/text processing using Lemmitization.py
@@ -1,6 +1,19 @@
+# first need to import Nltk
+
+import nltk
+# Need to download  stopword
+
+# Fromm corpus class import functinon stopwords
+
+from nltk.corpus import stopwords
+ 
+ #downloaded the Stopwords for the english
+ 
+n=stopwords.words("english")
+print(n)
 #text processing using Lemmitization
-from nltk.stem import WordNetLemmatizer
+# from nltk.stem import WordNetLemmatizer
 
-lemmatizer = WordNetLemmatizer()
+# lemmatizer = WordNetLemmatizer()
 
-print(lemmatizer.lemmatize("going"))
+# print(lemmatizer.lemmatize("going"))
